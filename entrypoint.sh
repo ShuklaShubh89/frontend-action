ea5611b36a88b000d91912b1b12e3008f87c22d2
Shubham Shukla <shubham.cored@gmail.com>
2022-08-28 13:16:33 +0530

    feat: inital commit

---
+++ entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_CLOUDFRONT_DISTRIBUTION" ]; then
+  echo "AWS_CLOUDFRONT_DISTRIBUTION is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+# Default to us-east-1 if AWS_REGION not set.
+if [ -z "$AWS_REGION" ]; then
+  AWS_REGION="us-east-1"
+fi
+
+aws configure --profile frontend-action <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+# Sync using our dedicated profile and suppress verbose messages.
+sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+            --profile s3-sync-action \
+            --no-progress"
+
+sh -c "aws cloudfront create-invalidation \
+            --distribution-id ${AWS_CLOUDFRONT_DISTRIBUTION} \
+            --paths "/img/*.png" "/img/*.jpg" "/js/*.js" "
+
+# Clear out credentials after we're done.
+# We need to re-run `aws configure` with bogus input instead of
+# deleting ~/.aws in case there are other credentials living there.
+# https://forums.aws.amazon.com/thread.jspa?threadID=148833
+aws configure --profile frontend-action <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
